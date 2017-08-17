588b0e2df68657aeb5595d8763addd68270adc82
jpushpendradolphin <anjanpushpendra@gmail.com>
2017-08-17 19:43:37 +0530

    added LoginService

---
+++ itjobs/target/itjobs/WEB-INF/classes/com/pusp/dao/LoginServiceI.java
@@ -0,0 +1,6 @@
+package com.pusp.dao;
+
+public interface LoginServiceI {
+	public boolean validateUser(String uname, String pass);
+}
+
---
+++ itjobs/target/itjobs/WEB-INF/classes/com/pusp/dao/LoginServiceIImpl.java
@@ -0,0 +1,8 @@
+package com.pusp.dao;
+
+public class LoginServiceIImpl implements LoginServiceI {
+	public boolean validateUser(String uname, String pass){
+		
+	}
+
+}
